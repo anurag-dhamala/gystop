587ca2fda55ec5cf231a07d9e0dce931944dc385
Anurag Dhamala <anuragdhamala8@gmail.com>
2022-10-24 21:11:38 +0545

    feat: new cpu struct acc. to sysinfo module

--- src-tauri/src/sys_info/cpu/mod.rs
+++ src-tauri/src/sys_info/cpu/mod.rs
@@ -1,47 +1,42 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct SingleCpu {
-    vendor_name: String,
-    model_name: String,
-    core_id: i32,
-    mhz: f64,
-    cache_size: String,
-    clock: String,
-    cpu_usage: String,
+pub struct SingleCpu {
+    name: String,
+    vendor_id: String,
+    brand_name: String,
+    mhz: u64,
+    cpu_usage: f32,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CpuData {
-    architecture: String,
-    byte_order: String,
-    operation_modes: String,
-    cpu_arr: Option<Vec<SingleCpu>>,
-}
-
-impl CpuData {
+impl SingleCpu {
     pub fn new() -> Self {
         Self {
-            architecture: "".to_string(),
-            byte_order: "".to_string(),
-            operation_modes: "".to_string(),
-            cpu_arr: None,
+            name: "".to_string(),
+            vendor_id: "".to_string(),
+            brand_name: "".to_string(),
+            mhz: 0,
+            cpu_usage: 0.0,
         }
     }
 
-    pub fn set_architecture(&mut self, architecture: String) {
-        self.architecture = architecture;
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
     }
 
-    pub fn set_byte_order(&mut self, byte_order: String) {
-        self.byte_order = byte_order;
+    pub fn set_vendor_id(&mut self, vendor_id: String) {
+        self.vendor_id = vendor_id;
     }
 
-    pub fn set_operation_modes(&mut self, operation_modes: String) {
-        self.operation_modes = operation_modes;
+    pub fn set_brand_name(&mut self, brand_name: String) {
+        self.brand_name = brand_name;
     }
 
-    pub fn set_cpu_arr(&mut self, cpu_arr: Option<Vec<SingleCpu>>) {
-        self.cpu_arr = cpu_arr;
+    pub fn set_mhz(&mut self, mhz: u64) {
+        self.mhz = mhz;
+    }
+
+    pub fn set_cpu_usage(&mut self, cpu_usage: f32) {
+        self.cpu_usage = cpu_usage;
     }
 }
