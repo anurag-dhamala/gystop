ca59590cfda3dc1e7de6ea2826b25a63d62fef06
Anurag Dhamala <anuragdhamala8@gmail.com>
2022-10-24 18:39:13 +0545

    feat: Sidebar component

---
+++ src/lib/MainContent/MainContent.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { InfoCategory } from '../../utils/InfoCategory';
+  import Cpu from './Cpu/Cpu.svelte';
+  import Devices from './Devices/Devices.svelte';
+
+  export let selectedItem;
+</script>
+
+{#if selectedItem.id === InfoCategory.CPU}
+  <Cpu />
+{:else if selectedItem === InfoCategory.DEVICES}
+  <Devices />
+{:else}
+  <div />
+{/if}
+
+<div />
--- src/lib/Sidebar/Sidebar.svelte
+++ src/lib/Sidebar/Sidebar.svelte
@@ -1,43 +1,47 @@
 <script lang="ts">
-    export let sidebarData;
-    export let selected;
-    export let onSelectItem;
+  export let sidebarData;
+  export let selected;
+  export let onSelectItem;
 </script>
 
-
 <div class="sidebar__container">
-    {#each sidebarData as item}
-        <div
-                class={item.id === selected.id ? "sidebar__item__selected": "sidebar__item"}
-                id={item.id}
-                on:click={()=>onSelectItem(item)}
-        >
-            {item.title}
-        </div>
-        {/each}
+  {#each sidebarData as item}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+      class={item.id === selected.id
+        ? 'sidebar__item__selected'
+        : 'sidebar__item'}
+      id={item.id}
+      on:click={() => onSelectItem(item)}
+    >
+      {item.title}
+    </div>
+  {/each}
 </div>
 
-
 <style>
-    .sidebar__container{
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        border-right: 1px solid gray;
-        gap: 50px;
-        width: 100%;
-        height: 100%;
-    }
+  .sidebar__container {
+    display: flex;
+    flex-direction: column;
+    justify-content: stretch;
+    border-right: 1px solid gray;
+    gap: 50px;
+    width: 100%;
+    align-items: stretch;
+    height: 80vh;
+  }
 
-    .sidebar__item{
-        cursor: pointer;
-    }
+  .sidebar__item {
+    cursor: pointer;
+    padding: 10px;
+  }
 
-    .sidebar__item__selected{
-        font-weight: bold;
-    }
+  .sidebar__item__selected {
+    font-weight: bold;
+    padding: 10px;
+  }
 
-    .sidebar__item:hover{
-        background: gray;
-    }
+  .sidebar__item:hover {
+    background: #e4e4e4;
+  }
 </style>
