ccf90141e5be45841313c4fd4865c678ba8ca389
Anurag Dhamala <anuragdhamala8@gmail.com>
2022-10-24 21:12:23 +0545

    fix: remove greet test cmd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,12 +8,6 @@ mod tauri_commands;
 
 use crate::tauri_commands::{get_cpu, get_devices, get_disks, get_memory, get_temperature};
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
