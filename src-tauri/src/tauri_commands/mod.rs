5b4d3c3757c174a79844c9527aaaa2149f528dd8
Anurag Dhamala <anuragdhamala8@gmail.com>
2022-10-24 18:48:24 +0545

    feat: tauri commands separated to new mod

---
+++ src-tauri/src/tauri_commands/mod.rs
@@ -0,0 +1,14 @@
+#[tauri::command]
+pub fn get_cpu() {}
+
+#[tauri::command]
+pub fn get_memory() {}
+
+#[tauri::command]
+pub fn get_disks() {}
+
+#[tauri::command]
+pub fn get_devices() {}
+
+#[tauri::command]
+pub fn get_temperature() {}
