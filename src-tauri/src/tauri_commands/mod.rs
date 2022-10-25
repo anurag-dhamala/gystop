4c41d5e1f4491b2683bf44247e7900386efa633e
Anurag Dhamala <anuragdhamala8@gmail.com>
2022-10-25 08:34:06 +0545

    feat: Implement tauri command for fetching disk details

--- src-tauri/src/tauri_commands/mod.rs
+++ src-tauri/src/tauri_commands/mod.rs
@@ -1,8 +1,8 @@
-//private functions to this module
-
 use crate::sys_info::cpu::SingleCpu;
-use sysinfo::{CpuExt, System, SystemExt};
+use crate::sys_info::disks::Disk;
+use sysinfo::{CpuExt, DiskExt, System, SystemExt};
 
+//private functions to this module
 fn get_sys_instance() -> System {
     let sys = System::new();
     sys
@@ -31,7 +31,24 @@ pub fn get_cpu() -> Vec<SingleCpu> {
 pub fn get_memory() {}
 
 #[tauri::command]
-pub fn get_disks() {}
+pub fn get_disks() -> Vec<Disk> {
+    let mut sys_instance = get_sys_instance();
+    sys_instance.refresh_disks();
+    sys_instance.refresh_disks_list();
+
+    let mut vec: Vec<Disk> = Vec::new();
+    for disk in sys_instance.disks() {
+        let mut single_disk = Disk::new();
+        single_disk.set_mount_point(disk.mount_point().to_path_buf());
+        single_disk.set_total_space(disk.total_space());
+        single_disk.set_available_space(disk.available_space());
+        single_disk.set_name(disk.name().to_os_string());
+        single_disk.set_is_removable(disk.is_removable());
+        vec.push(single_disk);
+    }
+    println!("{:#?}", vec);
+    vec
+}
 
 #[tauri::command]
 pub fn get_devices() {}
